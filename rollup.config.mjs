25084b808da6d340e6fd096a76a40f0dc5a4d8af
imtaotao <dwchentao@163.com>
2024-07-12 21:56:56 +0800

    chore: modify package.json exports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,7 +6,7 @@ import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 const { dirname: __dirname } = import.meta;
 
